f9da44a4f3df858d97d810f9fe68e61bd141fdad
icarus496 <bendobozy@gmail.com>
2016-12-03 13:49:16 -0500

    Changed scor sorry sam

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
-#import score
+import random
+import score
 screen_dimensions = [600,600]
 screen=pygame.display.set_mode(screen_dimensions)
 screencolor=(100,200,255)
@@ -11,7 +12,7 @@ def tracing(rectlist):
     screen.fill(screencolor)
     m=0 #timer, to fix a bug. It's there for a reason, trust me
     while True:
-        mousex = pygame.mouse.get_pos()[0]
+        mousex = pygame.mouse.get_pos()[0] #getting mouse x and y
         mousey = pygame.mouse.get_pos()[1]
 
         if pygame.mouse.get_pressed()[0] and m>=150:
@@ -19,10 +20,10 @@ def tracing(rectlist):
                 n=[rectlist,tracelist] #stuff to return
                 return n
             else:
-                tracelist.append(pygame.Rect(mousex, mousey, 10, 10))
+                tracelist.append(pygame.Rect(mousex, mousey, 10, 10)) #draw with the mouse
 
 
-        #Drawing rectangle
+        #Drawing stuff
         pygame.draw.rect(screen, (255, 0, 0), save)
         for item in range(len(rectlist)):
             pygame.draw.rect(screen, (0, 0, 0), rectlist[item])
@@ -37,3 +38,13 @@ def main(rectlist):
     tracing_and_stencil=tracing(rectlist)
     stencil=tracing_and_stencil[0]
     trace=tracing_and_stencil[1]
+    stencil_length=len(stencil) #stencil length
+    trace_length=len(trace) #trace length
+    length_diff=stencil_length-trace_length
+    temp_lendiff=length_diff
+    while temp_lendiff!=0:
+        index=random.randint(0,len(trace))
+        del(trace[index])
+        temp_lendiff-=1
+    for item in range(len(trace)):
+        score.score(trace,stencil)
